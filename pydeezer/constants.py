455fa093a4520ceb6a32814c6a6c2fc9676fc545
Chr1st <aaroncgonzales.dev@gmail.com>
2020-03-30 18:35:27 +0800

    added deezer api constants

---
+++ pydeezer/constants.py
@@ -0,0 +1,16 @@
+API_URL = "https://www.deezer.com/ajax/gw-light.php"
+MOBILE_API_URL = "https://api.deezer.com/1.0/gateway.php"
+LEGACY_API_URL = "https://api.deezer.com/"
+
+HTTP_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+    "Content-Language": "en-US",
+    "Cache-Control": "max-age=0",
+    "Accept": "*/*",
+    "Accept-Charset": "utf-8,ISO-8859-1;q=0.7,*;q=0.3",
+    "Accept-Language": "en-US,en;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Connection": 'keep-alive'
+}
+
+ALBUM_HOST = "https://e-cdns-images.dzcdn.net/images/cover/"
+ARTIST_HOST = "https://e-cdns-images.dzcdn.net/images/artist/"
