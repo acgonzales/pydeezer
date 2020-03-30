4c51a4aa9939f4dec1a750102435ec521adabced
Chr1st <aaroncgonzales.dev@gmail.com>
2020-03-30 18:35:12 +0800

    created Deezer models

---
+++ pydeezer/Deezer.py
@@ -0,0 +1,8 @@
+import constants
+
+class Deezer:
+    def __init__(self, arl=None):
+        pass
+
+    def loginViaArl(arl):
+        pass
---
+++ pydeezer/ens/User.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, id, name, arl, picture="https://e-cdns-images.dzcdn.net/images/user/250x250-000000-80-0-0.jpg"):
+        self.id = id
+        self.name = name
+        self.picture = picture
+        self.arl = arl
