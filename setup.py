55d6623a53c59b7321dfe41cf30c22aa889bff0b
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2020-05-05 17:10:51 +0800

    Solution to Deezer API patch, include genre in tags

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="py-deezer",
-    version="1.1.0",
+    version="1.1.1",
     author="Aaron Gonzales",
     author_email="aaroncgonzales.dev@gmail.com",
     description="A package to search and download musics on Deezer",
