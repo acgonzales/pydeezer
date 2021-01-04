35f4f4d2fac94d5b1eb7cbec1edbcfc73d52d007
Aaron Chazz Gonzales <aaroncgonzales.dev@gmail.com>
2021-01-05 01:21:12 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="py-deezer",
-    version="1.1.2",
+    version="1.1.3",
     author="Aaron Gonzales",
     author_email="aaroncgonzales.dev@gmail.com",
     description="A package to search and download musics on Deezer",
